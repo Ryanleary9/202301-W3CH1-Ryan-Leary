5877f65512fec37b7e02ee3cf697a3916d1f81cf
Ryan <ryanlearyzafra@gmail.com>
2023-01-30 20:59:59 +0100

    ✅ Add passed test

--- src/function.test.ts
+++ src/function.test.ts
@@ -1,1 +1,11 @@
-export class Personas {}
+import { Personas } from '../src/function';
+
+describe('Given Personas class', () => {
+  const r = new Personas('Juan', 'monkey', 32, true);
+  test('When entered the names should give back the names in order', () => {
+    expect(r).toHaveProperty('names');
+    expect(r).toHaveProperty('bloodline');
+    expect(r).toHaveProperty('age');
+    expect(r).toHaveProperty('life');
+  });
+});
--- src/function.ts
+++ src/function.ts
@@ -1,1 +1,22 @@
-import { Personas } from './function.test';
+export class Personas {
+  names: string;
+  bloodline: string;
+  age: number;
+  life: boolean;
+  constructor(names: string, bloodline: string, age: number, life: boolean) {
+    this.names = names;
+    this.bloodline = bloodline;
+    this.age = age;
+    this.life = life;
+  }
+}
+
+// Class kingRank extends Personas {
+//   reigntime: number;
+//   message: string;
+//   constructor(reigntime: number, message: string) {
+//     super(names, bloodline, age, life);
+//     this.reigntime = reigntime;
+//     this.message = message;
+//   }
+// }
